9d4ae580d06141749df2426ba00780fc4cb867cc
nezam0003 <fhameedk.nezam@gmail.com>
2021-03-30 21:32:11 +0600

    useState:  simple login form

--- learn-react/src/Hooks/Login-Form/App.jsx
+++ learn-react/src/Hooks/Login-Form/App.jsx
@@ -1,10 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [allEntry, setAllEntry] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newEntry = {
+      email: email,
+      password: password,
+    };
+    setAllEntry([...allEntry, newEntry]);
+    // alert("form submission successfull");
+  };
   return (
-    <div>
-      <button className="btn btn-primary">hello</button>
-    </div>
+    <>
+      <div className="row">
+        <div className="col-5 mt-5 mx-auto">
+          <div className="card bg-dark text-light">
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="email" className="form-label">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    id="email"
+                    autoComplete="off"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    id="password"
+                    autoComplete="off"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <button type="submit" className="btn btn-secondary">
+                  Login
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-7 mx-auto mt-3">
+          {allEntry.map((user, index) => {
+            return (
+              <div className="card bg-primary text-light">
+                <div className="card-body d-flex justify-content-between">
+                  <p>Email: {user.email} </p>
+                  <p>Password: {user.password} </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 };
 
