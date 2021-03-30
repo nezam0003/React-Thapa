7556fdb1f0aa8b294fb770906828b0f71830a35b
nezam0003 <fhameedk.nezam@gmail.com>
2021-03-30 15:41:01 +0600

    initial setup

---
+++ learn-react/src/Hooks/Login-Form/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <button className="btn btn-primary">hello</button>
+    </div>
+  );
+};
+
+export default App;
---
+++ learn-react/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+/********* Hooks - Login-form ***********/
+import App from "./Hooks/Login-Form/App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
