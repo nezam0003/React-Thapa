1f791c085fcaeb529310079d5fc5de7422665a1e
nezam0003 <fhameedk.nezam@gmail.com>
2021-03-31 12:10:06 +0600

    useState:  basic login form

--- learn-react/src/Hooks/Login-Form/App.jsx
+++ learn-react/src/Hooks/Login-Form/App.jsx
@@ -1,22 +1,38 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [allEntry, setAllEntry] = useState([]);
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    phone: "",
+  });
+  const [record, setRecord] = useState([]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (email && password) {
-      const newEntry = {
-        email,
-        password,
+    if (user.name && user.email && user.password && user.phone) {
+      const newUser = {
+        ...user,
+        id: new Date().getTime().toString(),
       };
-      setAllEntry([...allEntry, newEntry]);
-      setEmail("");
-      setPassword("");
+      setRecord([...record, newUser]);
+      setUser({
+        name: "",
+        email: "",
+        password: "",
+        phone: "",
+      });
     } else {
-      alert("please fill the data");
+      alert("please fill the input fields");
     }
   };
   return (
@@ -27,6 +43,20 @@ const App = () => {
             <div className="card-body">
               <form onSubmit={handleSubmit}>
                 <div className="form-group">
+                  <label htmlFor="name" className="form-label">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="name"
+                    id="name"
+                    autoComplete="off"
+                    value={user.name}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="form-group">
                   <label htmlFor="email" className="form-label">
                     Email
                   </label>
@@ -36,8 +66,8 @@ const App = () => {
                     name="email"
                     id="email"
                     autoComplete="off"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    value={user.email}
+                    onChange={handleChange}
                   />
                 </div>
                 <div className="form-group">
@@ -50,8 +80,22 @@ const App = () => {
                     name="password"
                     id="password"
                     autoComplete="off"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    value={user.password}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="phone" className="form-label">
+                    Phone
+                  </label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    name="phone"
+                    id="phone"
+                    autoComplete="off"
+                    value={user.phone}
+                    onChange={handleChange}
                   />
                 </div>
                 <button type="submit" className="btn btn-secondary">
@@ -63,13 +107,16 @@ const App = () => {
         </div>
       </div>
       <div className="row">
-        <div className="col-7 mx-auto mt-3">
-          {allEntry.map((user, index) => {
+        <div className="col-9 mx-auto mt-3">
+          {record.map((info) => {
+            const { name, email, password, phone, id } = info;
             return (
-              <div key={index} className="card bg-primary text-light">
-                <div className="card-body d-flex justify-content-between">
-                  <p>Email: {user.email} </p>
-                  <p>Password: {user.password} </p>
+              <div className="card bg-primary text-light" key={id}>
+                <div className="card-body d-flex justify-content-between align-items-center">
+                  <p>{name}</p>
+                  <p>{email}</p>
+                  <p>{password}</p>
+                  <p>{phone}</p>
                 </div>
               </div>
             );
